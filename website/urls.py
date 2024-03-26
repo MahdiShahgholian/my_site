a2f6b539a55b2cda722a41202206c16c02659d8b
MahdiShahgholian <mahdi.sha6773@gmail.com>
2024-03-25 17:06:46 -0700

    create html for home, about, contact

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,11 @@
 from django.urls import path
 from .views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('', index_view),
+    path('', index_view, name = 'index'),
+    path('about', about_view, name= 'about'),
+    path('contact', contact_view, name= 'contact'),
 ]
 
