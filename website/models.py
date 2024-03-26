5f81bd17ae03b81c8c7ee19cd842362aaaeb1fa7
MahdiShahgholian <mahdi.sha6773@gmail.com>
2024-03-26 15:37:16 -0700

    post model in admin

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Content(models.Model):
+    
+    name = models.CharField(max_length=20)
+    email_address = models.EmailField(max_length=20)
+    subject = models.CharField(max_length=50)
+    message = models.TextField(max_length=1000)
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
